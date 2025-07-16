76db88a5ea60b1fde187bf4fa5ca074c9ddeaec0
Patricio Nuñez <patricionunez@my-mac.local>
2025-07-16 13:01:44 -0300

    Update tests path on script and use setUp and tearDown

--- integration_test/test/app_test.dart
+++ integration_test/test/app_test.dart
@@ -8,46 +8,44 @@ import '../utils/utils.dart';
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  group('AppTest', () {
-    const testName = 'Tap on the floating action button, verify counter';
-    late final TestResults report;
-    late final String testUUID;
-    setUpAll(() {
-      report = TestResults(testName);
-      testUUID = report.generateAllureReportId();
+  const testName = 'Tap on the floating action button, verify counter';
+  late final TestResults report;
+  late final String testUUID;
+  setUp(() {
+    report = TestResults(testName);
+    testUUID = report.generateAllureReportId();
+  });
+
+  testWidgets(testName, (tester) async {
+    await report.addStep('Load app widget', () async {
+      await tester.pumpWidget(MyApp());
     });
 
-    testWidgets(testName, (tester) async {
-      await report.addStep('Load app widget', () async {
-        await tester.pumpWidget(MyApp());
-      });
-
-      await report.addStep('Verify counter starts at 0', () async {
-        expect(find.text('0'), findsOneWidget);
-      });
-
-      final actionButtonFinder = find.byKey(ValueKey('increment'));
-      await report.addStep('Finds the floating action button to tap on', () {
-        expect(actionButtonFinder, findsOneWidget);
-      });
-
-      await report.addStep('Tap on the floating action button', () async {
-        await tester.tap(actionButtonFinder);
-      });
-
-      await report.addStep('Pump and settle the widget', () async {
-        await tester.pumpAndSettle();
-      });
-
-      await report.addStep('Verify counter increments by 1', () {
-        expect(find.text('1'), findsOneWidget);
-      });
-
-      report.passTest();
+    await report.addStep('Verify counter starts at 0', () async {
+      expect(find.text('0'), findsOneWidget);
     });
 
-    tearDown(() async {
-      await report.uploadReportToGoogleCloudStorage(testUUID);
+    final actionButtonFinder = find.byKey(ValueKey('increment'));
+    await report.addStep('Finds the floating action button to tap on', () {
+      expect(actionButtonFinder, findsOneWidget);
     });
+
+    await report.addStep('Tap on the floating action button', () async {
+      await tester.tap(actionButtonFinder);
+    });
+
+    await report.addStep('Pump and settle the widget', () async {
+      await tester.pumpAndSettle();
+    });
+
+    await report.addStep('Verify counter increments by 1', () {
+      expect(find.text('1'), findsOneWidget);
+    });
+
+    report.passTest();
+  });
+
+  tearDown(() async {
+    await report.uploadReportToGoogleCloudStorage(testUUID);
   });
 }
